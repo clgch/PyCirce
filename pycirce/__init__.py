07811724d2c3585837daf7d673d8d0f9db384c13
GAUCHY Clement 261643 <cg261643@is248572.intra.cea.fr>
2025-11-18 09:15:57 +0100

    Add EM algorithm for diagonal covariance matrix

--- pycirce/__init__.py
+++ pycirce/__init__.py
@@ -1,7 +1,11 @@
 """otquickmodule module"""
 
 from .CirceECME import CirceECME
+from .CirceEM import CirceEM
+from .CirceEMdiag import CirceEMdiag
 __all__ = [
 "CirceECME",
+"CirceEM",
+"CirceEMdiag"
 ]
 __version__ = "0.0.1"
