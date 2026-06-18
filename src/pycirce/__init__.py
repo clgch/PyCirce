67caec784df7b641cc4d4a57bd438e4b79406364
GAUCHY Clement 261643 <cg261643@is248572.intra.cea.fr>
2026-06-18 16:12:33 +0200

    WIP: CIRCE ECME ridge, to be tested

--- src/pycirce/__init__.py
+++ src/pycirce/__init__.py
@@ -4,13 +4,17 @@ from .CirceECME import CirceECME
 from .CirceEM import CirceEM
 from .CirceEMdiag import CirceEMdiag
 from .CirceECMEdiag import CirceECMEdiag
+from .CirceECMERidge import CirceECMERidge
 from .NoisyCirceDiag import NoisyCirceDiag
+from .CirceREML import CirceREML
 
 __all__ = [
     "CirceECME",
+    "CirceECMERidge",
     "CirceEM",
     "CirceEMdiag",
     "CirceECMEdiag",
     "NoisyCirceDiag",
+    "CirceREML"
 ]
 __version__ = "0.0.1"
